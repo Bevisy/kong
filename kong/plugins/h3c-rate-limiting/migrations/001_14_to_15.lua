7ecf58e0185d516a823febb62c477799e9b8f3a5
z15085 <zhang.binbin@h3c.com>
2020-03-23 17:25:35 +0800

    fix(plugin) Correct the sql function name

--- kong/plugins/h3c-rate-limiting/migrations/001_14_to_15.lua
+++ kong/plugins/h3c-rate-limiting/migrations/001_14_to_15.lua
@@ -12,10 +12,10 @@ return {
     teardown = function(connector)
       assert(connector:connect_migrations())
       assert(connector:query([[
-        DROP FUNCTION IF EXISTS "increment_rate_limits_api" (UUID, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, INTEGER) CASCADE;
-        DROP FUNCTION IF EXISTS "increment_rate_limits" (UUID, TEXT, TEXT, TIMESTAMP WITHOUT TIME ZONE, INTEGER) CASCADE;
-        DROP FUNCTION IF EXISTS "increment_rate_limits" (UUID, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, INTEGER) CASCADE;
-        DROP FUNCTION IF EXISTS "increment_rate_limits" (UUID, UUID, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, INTEGER) CASCADE;
+        DROP FUNCTION IF EXISTS "h3c_increment_rate_limits_api" (UUID, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, INTEGER) CASCADE;
+        DROP FUNCTION IF EXISTS "h3c_increment_rate_limits" (UUID, TEXT, TEXT, TIMESTAMP WITHOUT TIME ZONE, INTEGER) CASCADE;
+        DROP FUNCTION IF EXISTS "h3c_increment_rate_limits" (UUID, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, INTEGER) CASCADE;
+        DROP FUNCTION IF EXISTS "h3c_increment_rate_limits" (UUID, UUID, TEXT, TEXT, TIMESTAMP WITH TIME ZONE, INTEGER) CASCADE;
       ]]))
     end,
   },
